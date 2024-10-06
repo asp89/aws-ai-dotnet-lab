40bebefe1ef765f141b7173f98651996eed31633
Anjaneya <anjaneya.singh94@gmail.com>
2024-10-06 14:01:35 +0530

    dotnet format

--- comprehend/comprehend-action-lambda/src/Models/ActionTypeConstants.cs
+++ comprehend/comprehend-action-lambda/src/Models/ActionTypeConstants.cs
@@ -3,9 +3,14 @@ namespace Models;
 public static class ActionTypeConstants
 {
     public const string DetectDominantLanguage = "detectdominantlanguage";
+
     public const string DetectEntities = "detectentities";
+
     public const string DetectKeyPhrases = "detectkeyphrases";
-    public const string DetectSentiment = "detectsentiment";
-    public const string DetectSyntaxAnalysis = "detectsyntaxanalysis";
+
     public const string DetectPiiEntities = "detectpiientities";
+
+    public const string DetectSentiment = "detectsentiment";
+
+    public const string DetectSyntaxAnalysis = "detectsyntaxanalysis";
 }
